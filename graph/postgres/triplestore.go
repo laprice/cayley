9dc6a3b007204097cac662a0e9347931967baa28
Larry Price <larry@industrialintellect.com>
2014-12-09 02:21:42 -0800

    trying to enforce a uniform naming scheme.
    TODO:
    
    implement Horizon and other missing functions of the QuadStore interface.

--- graph/postgres/triplestore.go
+++ graph/postgres/triplestore.go
@@ -59,8 +59,8 @@ CREATE INDEX quad_prov_idx ON quads(prov);
 func createNewPostgresGraph(addr string, options graph.Options) error {
 	t, err := newQuadStore(addr, options)
 	if err == nil {
-		defer t.Close()
-		_, err = t.(*QuadStore).db.Exec(pgSchema)
+		defer q.Close()
+		_, err = q.(*QuadStore).db.Exec(pgSchema)
 	}
 	return err
 }
@@ -78,19 +78,19 @@ func newQuadStore(addr string, options graph.Options) (graph.QuadStore, error) {
 	}, nil
 }
 
-func (t *QuadStore) getOrCreateNode(name string) int64 {
-	val, ok := t.idCache.RevGet(name)
+func (q *QuadStore) getOrCreateNode(name string) int64 {
+	val, ok := q.idCache.RevGet(name)
 	if ok {
 		return val
 	}
 
-	r := t.db.QueryRowx("SELECT id FROM nodes WHERE name=$1::text;", name)
+	r := q.db.QueryRowx("SELECT id FROM nodes WHERE name=$1::text;", name)
 	err := r.Scan(&val)
 	if err != nil {
 		if err != sql.ErrNoRows {
 			glog.Fatalln(err.Error())
 		} else {
-			r = t.db.QueryRowx("INSERT INTO nodes (name) VALUES ($1) RETURNING id;", name)
+			r = q.db.QueryRowx("INSERT INTO nodes (name) VALUES ($1) RETURNING id;", name)
 			err = r.Scan(&val)
 			if err != nil {
 				glog.Fatalln(err.Error())
@@ -98,43 +98,43 @@ func (t *QuadStore) getOrCreateNode(name string) int64 {
 		}
 	}
 
-	t.idCache.Put(val, name)
+	q.idCache.Put(val, name)
 	return val
 }
 
 // Add a quad to the store.
-func (t *QuadStore) AddQuad(x *graph.Quad) {
-	sid := t.getOrCreateNode(x.Subject)
-	pid := t.getOrCreateNode(x.Predicate)
-	oid := t.getOrCreateNode(x.Object)
+func (q *QuadStore) AddQuad(x *graph.Quad) {
+	sid := q.getOrCreateNode(x.Subject)
+	pid := q.getOrCreateNode(x.Predicate)
+	oid := q.getOrCreateNode(x.Object)
 	if x.Provenance != "" {
-		cid := t.getOrCreateNode(x.Provenance)
-		t.db.MustExec(`INSERT INTO quads (subj, pred, obj, prov) VALUES ($1,$2,$3,$4);`, sid, pid, oid, cid)
+		cid := q.getOrCreateNode(x.Provenance)
+		q.db.MustExec(`INSERT INTO quads (subj, pred, obj, prov) VALUES ($1,$2,$3,$4);`, sid, pid, oid, cid)
 	} else {
-		t.db.MustExec(`INSERT INTO quads (subj, pred, obj, prov) VALUES ($1,$2,$3,NULL);`, sid, pid, oid)
+		q.db.MustExec(`INSERT INTO quads (subj, pred, obj, prov) VALUES ($1,$2,$3,NULL);`, sid, pid, oid)
 	}
 }
 
 // Add a set of quads to the store, atomically if possible.
-func (t *QuadStore) AddQuadSet(xset []*graph.Quad) {
-	t.db.MustExec("BEGIN; SET CONSTRAINTS ALL DEFERRED;")
+func (q *QuadStore) AddQuadSet(xset []*graph.Quad) {
+	q.db.MustExec("BEGIN; SET CONSTRAINTS ALL DEFERRED;")
 	// TODO: multi-INSERT or COPY FROM
 	for _, x := range xset {
-		t.AddQuad(x)
+		q.AddQuad(x)
 	}
-	t.db.MustExec("COMMIT;")
+	q.db.MustExec("COMMIT;")
 }
 
 // Removes a quad matching the given one  from the database,
 // if it exists. Does nothing otherwise.
-func (t *QuadStore) RemoveQuad(x *graph.Quad) {
+func (q *QuadStore) RemoveQuad(x *graph.Quad) {
 	if x.Provenance != "" {
-		t.db.MustExec(`DELETE FROM quads USING nodes s, nodes p, nodes o, nodes c
+		q.db.MustExec(`DELETE FROM quads USING nodes s, nodes p, nodes o, nodes c
 		WHERE s.name=$1::text AND p.name=$2::text AND o.name=$3::text AND c.name=$4::text
 		AND subj=s.id AND obj=o.id AND pred=p.id AND prov=c.id;`,
 			x.Subject, x.Predicate, x.Object, x.Provenance)
 	} else {
-		t.db.MustExec(`DELETE FROM quads USING nodes s, nodes p, nodes o
+		q.db.MustExec(`DELETE FROM quads USING nodes s, nodes p, nodes o
 		WHERE s.name=$1::text AND p.name=$2::text AND o.name=$3::text
 		AND subj=s.id AND obj=o.id AND pred=p.id AND prov IS NULL;`,
 			x.Subject, x.Predicate, x.Object)
@@ -142,47 +142,47 @@ func (t *QuadStore) RemoveQuad(x *graph.Quad) {
 }
 
 // Given an opaque token, returns the quad for that token from the store.
-func (t *QuadStore) Quad(tid graph.Value) (tr *graph.Quad) {
+func (q *QuadStore) Quad(tid graph.Value) (qd *graph.Quad) {
 	ok := false
 	gotAll := true
-	tr = &graph.Quad{}
-	trv := tid.(QuadValue)
+	qd = &graph.Quad{}
+	qv := tid.(QuadValue)
 
-	tr.Subject, ok = t.idCache.Get(trv[1])
+	qd.Subject, ok = q.idCache.Get(qv[1])
 	gotAll = gotAll && ok
-	tr.Predicate, ok = t.idCache.Get(trv[2])
+	qd.Predicate, ok = q.idCache.Get(qv[2])
 	gotAll = gotAll && ok
-	tr.Object, ok = t.idCache.Get(trv[3])
+	qd.Object, ok = q.idCache.Get(qv[3])
 	gotAll = gotAll && ok
-	if trv[4] != -1 {
-		tr.Provenance, ok = t.idCache.Get(trv[4])
+	if qv[4] != -1 {
+		qd.Provenance, ok = q.idCache.Get(qv[4])
 		gotAll = gotAll && ok
 		if !gotAll {
-			r := t.db.QueryRowx(`SELECT s.name, p.name, o.name, c.name
-				FROM quads t, nodes s, nodes p, nodes o, nodes c
-				WHERE t.id=$1 AND t.subj=s.id AND t.pred=p.id AND t.obj=o.id AND t.prov=c.id;`, trv[0])
-			err := r.Scan(&tr.Subject, &tr.Predicate, &tr.Object, &tr.Provenance)
+			r := q.db.QueryRowx(`SELECT s.name, p.name, o.name, c.name
+				FROM quads q, nodes s, nodes p, nodes o, nodes c
+				WHERE q.id=$1 AND q.subj=s.id AND q.pred=p.id AND q.obj=o.id AND q.prov=c.id;`, qv[0])
+			err := r.Scan(&qd.Subject, &qd.Predicate, &qd.Object, &qd.Provenance)
 			if err != nil {
 				glog.Fatalln(err.Error())
 			}
-			t.idCache.Put(trv[1], tr.Subject)
-			t.idCache.Put(trv[2], tr.Predicate)
-			t.idCache.Put(trv[3], tr.Object)
-			t.idCache.Put(trv[4], tr.Provenance)
+			q.idCache.Put(qv[1], qd.Subject)
+			q.idCache.Put(qv[2], qd.Predicate)
+			q.idCache.Put(qv[3], qd.Object)
+			q.idCache.Put(qv[4], qd.Provenance)
 		}
 	} else {
-		tr.Provenance = ""
+		qd.Provenance = ""
 		if !gotAll {
-			r := t.db.QueryRowx(`SELECT s.name, p.name, o.name
-				FROM quads t, nodes s, nodes p, nodes o
-				WHERE t.id=$1 AND t.subj=s.id AND t.pred=p.id AND t.obj=o.id;`, trv[0])
-			err := r.Scan(&tr.Subject, &tr.Predicate, &tr.Object)
+			r := q.db.QueryRowx(`SELECT s.name, p.name, o.name
+				FROM quads q, nodes s, nodes p, nodes o
+				WHERE q.id=$1 AND q.subj=s.id AND q.pred=p.id AND q.obj=o.id;`, qv[0])
+			err := r.Scan(&qd.Subject, &qd.Predicate, &qd.Object)
 			if err != nil {
 				glog.Fatalln(err.Error())
 			}
-			t.idCache.Put(trv[1], tr.Subject)
-			t.idCache.Put(trv[2], tr.Predicate)
-			t.idCache.Put(trv[3], tr.Object)
+			q.idCache.Put(qv[1], qd.Subject)
+			q.idCache.Put(qv[2], qd.Predicate)
+			q.idCache.Put(qv[3], qd.Object)
 		}
 	}
 
@@ -191,8 +191,8 @@ func (t *QuadStore) Quad(tid graph.Value) (tr *graph.Quad) {
 
 // Given a direction and a token, creates an iterator of links which have
 // that node token in that directional field.
-func (ts *QuadStore) QuadIterator(dir graph.Direction, val graph.Value) graph.Iterator {
-	it := NewQuadIterator(ts, dir, val)
+func (qs *QuadStore) QuadIterator(dir graph.Direction, val graph.Value) graph.Iterator {
+	it := NewQuadIterator(qs, dir, val)
 	if it.size == 0 {
 		it.Close()
 		return iterator.NewNull()
@@ -201,16 +201,16 @@ func (ts *QuadStore) QuadIterator(dir graph.Direction, val graph.Value) graph.It
 }
 
 // Returns an iterator enumerating all nodes in the graph.
-func (ts *QuadStore) NodesAllIterator() graph.Iterator {
-	return NewNodeIterator(ts)
+func (qs *QuadStore) NodesAllIterator() graph.Iterator {
+	return NewNodeIterator(qs)
 }
 
 // Returns an iterator enumerating all links in the graph.
-func (ts *QuadStore) QuadsAllIterator() graph.Iterator {
-	return NewAllIterator(ts)
+func (qs *QuadStore) QuadsAllIterator() graph.Iterator {
+	return NewAllIterator(qs)
 }
 
-func (t *QuadStore) FixedIterator() graph.FixedIterator {
+func (q *QuadStore) FixedIterator() graph.FixedIterator {
 	return iterator.NewFixedIteratorWithCompare(func(a, b graph.Value) bool {
 		switch v := a.(type) {
 		case NodeValue:
@@ -229,26 +229,26 @@ func (t *QuadStore) FixedIterator() graph.FixedIterator {
 
 // Given a node ID, return the opaque token used by the QuadStore
 // to represent that id.
-func (t *QuadStore) ValueOf(name string) graph.Value {
-	res, ok := t.idCache.RevGet(name)
+func (q *QuadStore) ValueOf(name string) graph.Value {
+	res, ok := q.idCache.RevGet(name)
 	if ok {
 		return NodeValue(res)
 	}
 
-	r := t.db.QueryRowx("SELECT id FROM nodes WHERE name=$1;", name)
+	r := q.db.QueryRowx("SELECT id FROM nodes WHERE name=$1;", name)
 	err := r.Scan(&res)
 	if err != nil {
 		if err != sql.ErrNoRows {
 			glog.Fatalln(err.Error())
 		}
 	} else {
-		t.idCache.Put(res, name)
+		q.idCache.Put(res, name)
 	}
 	return NodeValue(res)
 }
 
 // Given an opaque token, return the node that it represents.
-func (t *QuadStore) NameOf(oid graph.Value) (res string) {
+func (q *QuadStore) NameOf(oid graph.Value) (res string) {
 	var nid int64
 	switch v := oid.(type) {
 	case int64:
@@ -257,26 +257,26 @@ func (t *QuadStore) NameOf(oid graph.Value) (res string) {
 		nid = int64(v)
 	}
 
-	val, ok := t.idCache.Get(nid)
+	val, ok := q.idCache.Get(nid)
 	if ok {
 		return val
 	}
 
-	r := t.db.QueryRowx("SELECT name FROM nodes WHERE id=$1;", nid)
+	r := q.db.QueryRowx("SELECT name FROM nodes WHERE id=$1;", nid)
 	err := r.Scan(&res)
 	if err != nil {
 		if err != sql.ErrNoRows {
 			glog.Fatalln(err.Error())
 		}
 	} else {
-		t.idCache.Put(nid, res)
+		q.idCache.Put(nid, res)
 	}
 	return
 }
 
-// Returns the number of quads currently stored.
-func (t *QuadStore) Size() (res int64) {
-	r := t.db.QueryRowx("SELECT COUNT(*) FROM quads;")
+// Returns qhe number of quads currently stored.
+func (q *QuadStore) Size() (res int64) {
+	r := q.db.QueryRowx("SELECT COUNT(*) FROM quads;")
 	err := r.Scan(&res)
 	if err != nil {
 		glog.Fatalln(err.Error())
@@ -286,15 +286,15 @@ func (t *QuadStore) Size() (res int64) {
 
 // Close the quad store and clean up. (Flush to disk, cleanly
 // sever connections, etc)
-func (t *QuadStore) Close() {
-	t.db.Close()
+func (q *QuadStore) Close() {
+	q.db.Close()
 }
 
-// Convienence function for speed. Given a quad token and a direction
+// Convenience function for speed. Given a quad token and a direction
 // return the node token for that direction. Sometimes, a QuadStore
 // can do this without going all the way to the backing store, and
 // gives the QuadStore the opportunity to make this optimization.
-func (t *QuadStore) QuadDirection(quad_id graph.Value, dir graph.Direction) graph.Value {
+func (q *QuadStore) QuadDirection(quad_id graph.Value, dir graph.Direction) graph.Value {
 	qv := quad_id.(QuadValue)
 	switch dir {
 	case graph.Subject:
